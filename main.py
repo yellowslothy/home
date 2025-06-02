bb38f0b9cbb34fa41b92d6d1fcd2c9e56598d006
yellowslothy <yellowslothy3@gmail.com>
2025-06-02 17:08:01 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
+st.set_page_config(
+    page_title="🌈 MBTI 직업 추천기 💼",
+    page_icon="🧠",
+    layout="wide"
+)
 import streamlit as st
 
 st.title('나의 첫 streamlit 프로젝트!')
 st.write('Hello steamlit')
 
 import streamlit as st
-
-st.set_page_config(
-    page_title="🌈 MBTI 직업 추천기 💼",
-    page_icon="🧠",
-    layout="wide"
-)
 
 st.markdown("""
     <style>
